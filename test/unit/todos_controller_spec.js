8aaecbd098d06f2c72374443677fea862d760124
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-04 18:08:07 +0530

    setting up karma jasmine phantomjs and coverage

---
+++ test/unit/todos_controller_spec.js
@@ -0,0 +1,25 @@
+describe('todosController', function () {
+    var $rootScope, createController;
+
+    beforeEach(module('todoApp'));
+    beforeEach(inject(function ($injector) {
+        // Get hold of a scope (i.e. the root scope)
+        $rootScope = $injector.get('$rootScope');
+        var $controller = $injector.get('$controller');
+        createController = function () {
+            return $controller('todosController', {'$scope': $rootScope});
+        };
+    }));
+
+    afterEach(function () {
+    });
+
+    it("Should be defined", function () {
+        var controller = createController();
+        expect(controller).toBeDefined();
+    });
+
+    it('should send todo to server', function () {
+        expect('foo2').toBe('foo2');
+    });
+});
