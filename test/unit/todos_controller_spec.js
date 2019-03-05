ed9f84bb45029a1271a0f9e6da75e8df79a29e5c
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-05 14:16:16 +0530

    specs in mid way

--- test/unit/todos_controller_spec.js
+++ test/unit/todos_controller_spec.js
@@ -1,25 +1,45 @@
 describe('todosController', function () {
-    var $rootScope, createController;
 
+    // var $httpBackend, $rootScope, createController, $scope ;
+    var ctrl, scope;
     beforeEach(module('todoApp'));
-    beforeEach(inject(function ($injector) {
+
+    beforeEach(inject(function (_$rootScope_, _$controller_) {
+
+        var $rootScope = _$rootScope_;
+        var $controller = _$controller_;
+
+        scope = $rootScope.$new();
+        ctrl = $controller('todosController', {'$scope': $rootScope});
+
+
+        // Set up the mock http service responses
+        // $httpBackend = $injector.get('$httpBackend');
+        // $httpBackend.when('GET', 'pages/createTodo.htm').respond({ body: '<html><body>Mock homepage</body></html>'});
+        // $httpBackend.when('GET', 'pages/index.htm').respond({ body: '<html><body>Mock Indexpage</body></html>'});
         // Get hold of a scope (i.e. the root scope)
-        $rootScope = $injector.get('$rootScope');
-        var $controller = $injector.get('$controller');
-        createController = function () {
-            return $controller('todosController', {'$scope': $rootScope});
-        };
+        // $rootScope = $injector.get('$rootScope');
     }));
 
     afterEach(function () {
+        // $httpBackend.verifyNoOutstandingExpectation();
+        // $httpBackend.verifyNoOutstandingRequest();
     });
 
-    it("Should be defined", function () {
-        var controller = createController();
-        expect(controller).toBeDefined();
+
+    it("todoscontroller should be defined", function () {
+        expect(ctrl).toBeDefined();
     });
 
-    it('should send todo to server', function () {
-        expect('foo2').toBe('foo2');
+    it("scope todo should be defined with empty title", function () {
+        var newTodo = scope.newTodo;
+        expect(newTodo).toBeDefined();
+        expect(newTodo.title).toBe('');
     });
+
+    it("scope todos should be defined with empty array", function () {
+        var todos = scope.todos;
+        expect(todos).toBeDefined();
+    });
+
 });
