932133f3c881131fdb2cff9261336e5d259b4582
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-05 15:52:08 +0530

    1. updated addTodo removed loadtods usage 2. updated deleteTodo removed location reload 3. updated toggleStatus update todo object from response in todos

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -11,28 +11,31 @@ todoApp.controller('todosController',
             $scope.newTodo = {title: ''};
 
             $scope.addTodo = function () {
-                Todo.save({todo: $scope.newTodo}).$promise.then(function () {
+                Todo.save({todo: $scope.newTodo}).$promise.then(function (response) {
+                    $scope.todos.push(response.todo);
                     $scope.newTodo = {title: ''};
                     notify({message: 'Todo created successfully.', duration: 1000})
-                    loadtodos();
                 });
             };
 
-            $scope.deleteTodo = function (todoId) {
+            $scope.deleteTodo = function (todo) {
+                var todoId = todo._id.$oid;
+                var todoIndex = $scope.todos.indexOf(todo);
+
                 Todo.destroy({id: todoId}).$promise.then(function () {
+                    $scope.todos.splice(todoIndex, 1);
                     notify({message: 'Todo deleted successfully.', duration: 1000})
-                    loadtodos();
                 });
             };
 
-            $scope.toggleStatus = function (todoId, status) {
-                var requestedStatus = 'finished';
-                if (status === 'finished') {
-                    requestedStatus = 'started'
-                }
+            $scope.toggleStatus = function (todo) {
+                var todoId = todo._id.$oid;
+                var todoIndex = $scope.todos.indexOf(todo);
+
+                var requestedStatus = (todo.status === 'finished') ? 'started' : 'finished';
                 Todo.toggleStatus({id: todoId},
-                    {id: todoId, status: requestedStatus}).$promise.then(function () {
-                    $location.path('/')
+                    {id: todoId, status: requestedStatus}).$promise.then(function (response) {
+                    $scope.todos[todoIndex] = response.todo
                     notify({message: 'Todo marked as ' + requestedStatus + '.', duration: 1000})
                 });
             };
